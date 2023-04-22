2663588686aace00b35dda193f863503b8ad1b38
Benny Mendoza <bennybc9387@gmail.com>
2023-04-22 17:57:32 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelector('.dropdown button').addEventListener('click', function () {
+	document.querySelector('.dropdown-content').classList.toggle('show');
+});
+
+// Close dropdown menu if user clicks outside of it
+window.addEventListener('click', function (event) {
+	if (!event.target.matches('.dropdown button')) {
+		var dropdowns = document.querySelectorAll('.dropdown-content');
+		for (var i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+});
+
+
+console.log('why is this not workign')
+
+document.querySelector('.dropdown').addEventListener('click', () => {
+    console.log('i have been clicked')
+})
+
